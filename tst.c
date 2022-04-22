8fb6b63eebb6cf9b85635cc92051f25931c9dbcb
ASDazey <adazey@deelinc.com>
2022-04-22 10:35:34 -0700

    Update tst to wait for child to exit before exiting itself

--- tst.c
+++ tst.c
@@ -1,7 +1,20 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
 
-int main(int argc, char **argv) {
-  system("tabbed st -w");
-  return 0;
+int main(void) {
+  char* args[4] = {"tabbed", "st", "-w", NULL};
+  pid_t child_pid;
+  int child_status;
+
+  child_pid = fork();
+  if(child_pid == 0) {
+    execvp(args[0], args);
+    exit(0);
+  }
+  else {
+    wait(&child_status);
+    return child_status;
+  }
 }
